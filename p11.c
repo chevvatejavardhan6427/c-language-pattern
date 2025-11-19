e27cf4eb4aa57767bed5824dafa728f971da9cea
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-11-19 07:33:40 +0530

    Create p11.c

---
+++ p11.c
@@ -0,0 +1,31 @@
+/* write a c program to print a number triangular program.
+   1 
+  2 2 
+ 3 3 3 
+4 4 4 4 
+
+sample input: 4
+sample output:
+Enter the number of rows: 
+
+   1 
+  2 2 
+ 3 3 3 
+4 4 4 4 
+*/
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number of rows:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++){
+            printf("%d ",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
